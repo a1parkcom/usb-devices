823291912279dc248643f3c14d59c7584f3cb936
pysashapy <sasha.2000ibr@gmail.com>
2023-01-23 07:50:20 +0300

    test

--- UsbDevices/Printers/tickets.py
+++ UsbDevices/Printers/tickets.py
@@ -1,71 +1,26 @@
-from time import strftime
+import time
 
 
 class Tickets:
-    def __init__(self, email, phone, money_h, parking_id):
-        self.email = email
-        self.phone = phone
-        self.url = f'https://a1park.com/parking/{parking_id}'
-        self.money_h = money_h
-        self.parking_id = parking_id
+    def __init__(self, ticket_ok, ticket_space):
+        self.ticket_ok = ticket_ok
+        self.ticket_space = ticket_space
 
-    def entry(self, url, order_id):
-        return f"""  
-<text pd=6 align=left>
-   <text>Парковка {self.parking_id} </text>
-   <text>Дата: {strftime("%d.%m.%y Время %H:%M")} </text> 
-   <text>ID заказа: <b>{order_id}</b> </text> 
-   
-   <text>Въезжая на паркинг вы соглащаетесь </text>
-   <text>с правилами пользования парковки </text>  
-   
-   <text>{self.url} </text> 
-   <text>Сервисный сбор НОП 3% </text>
-   <text>Стоимость <b>{self.money_h}</b> Руб./час </text>
-   
-   <text pd=6 size=10 align=center>!Внимание!</text>
-   <text pd=6 align=left></text>
-   <text>----------------------------------------</text> 
-   <text>Для выезда наведите камеру телефона</text> 
-   <text>на QR код и пройдите на страницу</text> 
-   <text>для оплаты.</text>
-   <text>После оплаты появится кнопка ВЫЕЗД. </text>
-   <text>Находясь ПЕРЕД ШЛАГБАУМОМ нажмите ее</text> 
-   <text>и шлагбаум откроется.</text> 
-   <text>----------------------------------------</text> 
-   
-   <text align=center><QR content={url}></text> 
-    <text pd=6 align=left></text>
-   <text>Телефон для справок:</text> 
-   <text><b>{self.phone}</b></text>
-   <text><b>{self.email}</b></text>
+        self.url = f''
 
+    def set_ticket_data(self, ticket: str, order) -> str:
+        data = {
+            "order_car": order.car_number,
+            "order_id": order.id,
+            "price": order.price,
+            "nop": 3,
+            "data": time.strftime("%d.%m.%y"),
+            "time": time.strftime("%H:%M"),
+        }
+        return ticket.format(**data)
 
+    def ok(self, order):
+        return self.set_ticket_data(self.ticket_ok, order)
 
-
-
-</text>
-                """
-
-    def notPlaces(self):
-        return f"""  
-<text pd=6 align=left>
-   <text>Парковка {self.parking_id} </text>
-   <text>Дата: {strftime("%d.%m.%y Время %H:%M")} </text>
-   <text>Въезжая на паркинг вы соглащаетесь </text>
-   <text>с правилами пользования парковки </text>  
-   <text>{self.url}</text> 
-   <text>Стоимость <b>{self.money_h}</b> Руб./час </text>
-   <text>Сервисный сбор НОП 3% </text>
-
-   <text>К сожалению нет свободных мест,</text>
-   <text>попробуйте чуть позже,</text>
-   <text>как места появятся - обязательно</text>
-   <text>вам сообщим!</text>
-   <text>или позвоните в call центр,</text>
-
-   <text>Телефон для справок:</text> 
-   <text><b>{self.phone}</b></text>
-   <text><b>{self.email}</b></text>
-</text>
-"""
+    def not_space(self, order):
+        return self.set_ticket_data(self.ticket_space, order)
